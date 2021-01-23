28068ac55754a4e5eb643620842f3790f6409b44
Megumikawa <megumi19901016@gmail.com>
2021-01-23 01:25:09 +0100

    change img name

--- script.js
+++ script.js
@@ -13,27 +13,25 @@ fieldImg.src = 'images/bg-field.jpg'
 let playerImg = document.createElement('img')
 playerImg.src = 'images/player-right.png'
 
-let cloudImg1 = document.createElement('img')
-cloudImg1.src = 'images/cloud.png'
+let cloudLeftImg = document.createElement('img')
+cloudLeftImg.src = 'images/cloud.png'
 
-let cloudImg2 = document.createElement('img')
-cloudImg2.src = 'images/cloud.png'
+let cloudMiddleImg = document.createElement('img')
+cloudMiddleImg.src = 'images/cloud.png'
 
-let cloudImg3 = document.createElement('img')
-cloudImg3.src = 'images/cloud.png'
+let cloudRightImg = document.createElement('img')
+cloudRightImg.src = 'images/cloud.png'
 
 function draw() {
   ctx.drawImage(backImg, 0, 0)
-  ctx.drawImage(cloudImg1, 70, 140 )
-  ctx.drawImage(cloudImg2, 180, 330 )
-  ctx.drawImage(cloudImg3, canvas.width - 160, 180 )
+  ctx.drawImage(cloudLeftImg, 70, 140 )
+  ctx.drawImage(cloudMiddleImg, 180, 330 )
+  ctx.drawImage(cloudRightImg, canvas.width - 160, 180 )
   ctx.drawImage(playerImg, 0, canvas.height - 165 )
   ctx.drawImage(fieldImg, 0, canvas.height - fieldImg.height)
   
   ctx.font = '20px verdana'
   ctx.fillText('score:' + score, canvas.width - 100, 50)
-
-  
 
 
 }
