09618f84946cd4211946c6cdb0fe00bbb2060246
Megumikawa <megumi19901016@gmail.com>
2021-01-25 16:00:15 +0100

    finised player jump end

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let ctx = canvas.getContext('2d')
 canvas.style.border = '2px solid black'
 let intervalId = 0
 let score = 0
+
 // let currentTime = 0
 
 let backImg = document.createElement('img')
@@ -24,11 +25,37 @@ let coinImg = document.createElement('img')
 coinImg.src = 'images/coin.png'
 
 let rocks = [{x:canvas.width - 100, y:canvas.height - 168 }]
+let coin1 = [{x:40, y: 40 }]
 
 let playerX = 200  //start position
 let playerY = canvas.height - 165  //start position
 let vplayerY = 0  //speed
 let isJump = false  // jump or not jump
+
+
+// --------------------------------------
+function draw() {
+  ctx.drawImage(backImg, 0, 0)
+  
+  for (let i = 0; i < rocks.length; i++) {
+    ctx.drawImage(rockImg, rocks[i].x, rocks[i].y)
+    rocks[i].x--
+    if(rocks[i].x == 0) {
+      rocks.push({
+        x: canvas.width - 100,
+        y: canvas.height - 168
+      })
+    }
+  }
+  
+  ctx.drawImage(playerImg, playerX, playerY)
+  ctx.drawImage(coinImg, 20, 0)
+  ctx.drawImage(fieldImg, 0, canvas.height - fieldImg.height)
+  ctx.font = '20px verdana'
+  ctx.fillText('SCORE:' + score, canvas.width - 140, 50)
+  ctx.fillText('TIME:' + currentTime, canvas.width - 140, 80)
+}
+// --------------------------------------
 
 
 // --------Player----------
@@ -39,92 +66,49 @@ function handlekeydown(event) {
   event.preventDefault(); //
   input_key_buffer[event.keyCode] = true;
 }
-
 window.addEventListener("keyup", handlekeyup)
 function handlekeyup(event) {
   event.preventDefault()
   input_key_buffer[event.keyCode] = false
 }
-
 window.addEventListener("load", update);
 function update() {
   ctx.drawImage(playerImg, playerX, playerY)
   ctx.clearRect(0, 0, canvas.width, canvas.height)
+  let fieldY = canvas.height - fieldImg.height
   
   if (input_key_buffer[37]) {    //left
     playerX = playerX - 10
   } 
-  else if (input_key_buffer[39]) {    //right
+  if (input_key_buffer[39]) {    //right
     playerX = playerX + 10
   } 
-  else if (input_key_buffer[38]) {    //jump
+  if (input_key_buffer[38]) {    //jump
     vplayerY = -8;
     isJump = true;
-    playerLeftImg
   }
-  else if (isJump) {
+  if (isJump) {
+    if (playerY + playerImg.height > fieldY) {
+      playerY = fieldY - playerImg.height
+      isJump = false
+    }
+    else {
     playerY = playerY + vplayerY
     vplayerY = vplayerY + 0.5
+    }
   }
-  // else if(playerY) {
-  //   + playerY = 
-  // }
-
   window.requestAnimationFrame(update)
 }
-
-// function keydown(event) {
-//   let movement = event.keyCode
-//   switch (movement) {
-//     case 37: playerX -= 10; //left
-//       break;
-//     case 39: playerX += 10; //right
-//       break;
-//     case 38: playerY -= 10;  //up
-//       break;
-//   }
-// ↑↑ can't jump
-// }
 // --------------------------
 
 //----------coin-----------
-
-
-
-function draw() {
-  ctx.drawImage(backImg, 0, 0)
-  
-
-  for (let i = 0; i < rocks.length; i++) {
-    ctx.drawImage(rockImg, rocks[i].x, rocks[i].y)
-    rocks[i].x--
-
-    if(rocks[i].x == 0) {
-      rocks.push({
-        x: canvas.width - 100,
-        y: canvas.height - 168
-      })
-    }
-// GAME OVER condition
-    // if() {
-    //   clearInterval()
-    //   alert('game over')
-    // }
-
-  }
-
-  ctx.drawImage(playerImg, playerX, playerY)
-  ctx.drawImage(coinImg, 20, 0)
-  ctx.drawImage(fieldImg, 0, canvas.height - fieldImg.height)
-
-  ctx.font = '20px verdana'
-  ctx.fillText('SCORE:' + score, canvas.width - 140, 50)
-
-  ctx.font = '20px verdana'
-  ctx.fillText('TIME:' + currentTime, canvas.width - 140, 80)
-
+coinImg.onload = function (){
+  getComputedStyle.drawImage(coinImg,)
 }
 
+
+
+//----------------------------------
 // let i = 0
 //   let intervalIdEnd = setInterval(function(){
 //   i++
@@ -173,5 +157,3 @@ intervalId = setInterval( () => {
 // window.addEventListener('load', () => {
 //   countdown()
 // })
-
-
